b1a05f6cbd9cb0d2b0ecb96acbd718075f7387bb
yousufatef <joeatef769@gmail.com>
2025-05-07 23:25:33 +0300

    fix: update public route matcher to include '/api/clerk' instead of '/api/webhooks'

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -4,7 +4,7 @@ const isPublicRoute = createRouteMatcher([
     '/sign-in(.*)',
     '/sign-up(.*)',
     '/',
-    '/api/webhooks'
+    '/api/clerk'
 ])
 export default clerkMiddleware(async (auth, req) => {
     if (!isPublicRoute(req)) {
