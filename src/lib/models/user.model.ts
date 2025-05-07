0280972f154340cd04891f5fa0b6c0c1f542f96f
yousufatef <joeatef769@gmail.com>
2025-05-08 01:09:00 +0300

    feat: implement user management with MongoDB integration for Clerk webhooks

---
+++ src/lib/models/user.model.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+    clerkId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    profilePicture: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+}, { timestamps: true });
+
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+export default User;
---
+++ src/types/user.type.ts
@@ -0,0 +1,9 @@
+export interface IUser {
+    id: string | undefined;
+    first_name: string | null;
+    last_name: string | null;
+    image_url: string | null;
+    username: string | null;
+    email_addresses: { email_address: string }[];
+    isAdmin?: boolean;
+}
