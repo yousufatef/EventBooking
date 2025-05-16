518262ccf62ff74814f03ffac9e6e92b949c1382
yousufatef <joeatef769@gmail.com>
2025-05-16 22:10:37 +0300

    feat: improve layout and styling of EventDetails component for better user experience

--- src/app/(root)/events/[id]/page.tsx
+++ src/app/(root)/events/[id]/page.tsx
@@ -12,70 +12,68 @@ const EventDetails = async ({ params }: { params: Promise<{ id: string }> }) => 
     const formattedEndDate = format(new Date(event.endDateTime), "PP");
 
     return (
-        <section className="bg-primary-50 bg-dotted-pattern bg-contain py-10 px-4 md:px-10">
-            <div className="container grid  grid-cols-1 md:grid-cols-2 gap-8 lg:gap-2 rounded-2xl overflow-hidden">
-                <img
-                    src={event.imageUrl}
-                    alt={event.title}
-                    className="w-[500px] h-[500px] rounded-sm object-cover"
-                />
-                <div className="flex flex-col gap-8">
-                    <div className="flex flex-col gap-4">
-                        <div className="flex flex-wrap items-center gap-4">
-                            <span className="bg-green-100 text-green-700 px-4 lg:px-6 py-[6px] rounded-[12px]">
-                                {event.isFree ? "Free" : `$${event.price}`}
-                            </span>
-                        </div>
-                        <h2 className="text-3xl font-bold text-gray-900">{event.title}</h2>
+        <section className="container grid  grid-cols-1 md:grid-cols-2 gap-8 lg:gap-2 rounded-2xl overflow-hidden">
+            <img
+                src={event.imageUrl}
+                alt={event.title}
+                className="w-[500px] h-[500px] rounded-sm object-cover"
+            />
+            <div className="flex flex-col gap-8">
+                <div className="flex flex-col gap-4">
+                    <div className="flex flex-wrap items-center gap-4">
+                        <span className="bg-green-100 text-green-700 px-4 lg:px-6 py-[6px] rounded-[12px]">
+                            {event.isFree ? "Free" : `$${event.price}`}
+                        </span>
                     </div>
-                    <div className="flex flex-col gap-5 text-gray-700">
-                        <div className="flex items-center gap-2 text-sm">
-                            <div className="flex h-7 w-7 items-center justify-center rounded-full bg-primary/5">
-                                <MapPin className="h-4 w-4 text-primary" />
-                            </div>
-                            <span className="text-gray-700 dark:text-gray-300">{event.location}</span>
+                    <h2 className="text-3xl font-bold text-gray-900">{event.title}</h2>
+                </div>
+                <div className="flex flex-col gap-5 text-gray-700">
+                    <div className="flex items-center gap-2 text-sm">
+                        <div className="flex h-7 w-7 items-center justify-center rounded-full bg-primary/5">
+                            <MapPin className="h-4 w-4 text-primary" />
                         </div>
-                        <div className="flex items-center gap-2 text-sm">
-                            <div className="flex h-7 w-7 items-center justify-center rounded-full bg-primary/5">
-                                <Calendar className="h-4 w-4 text-primary" />
-                            </div>
-                            <span className="text-gray-700 dark:text-gray-300">{formattedStartDate} - {formattedEndDate}</span>
-                        </div>
+                        <span className="text-gray-700 dark:text-gray-300">{event.location}</span>
                     </div>
-                    <div className="flex flex-col gap-3">
-                        <h3 className="text-xl font-semibold text-gray-800">
-                            {"What You'll Learn"}
-                        </h3>
+                    <div className="flex items-center gap-2 text-sm">
+                        <div className="flex h-7 w-7 items-center justify-center rounded-full bg-primary/5">
+                            <Calendar className="h-4 w-4 text-primary" />
+                        </div>
+                        <span className="text-gray-700 dark:text-gray-300">{formattedStartDate} - {formattedEndDate}</span>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-3">
+                    <h3 className="text-xl font-semibold text-gray-800">
+                        {"What You'll Learn"}
+                    </h3>
 
-                        <p className="text-gray-700 leading-relaxed">{event.description}</p>
+                    <p className="text-gray-700 leading-relaxed">{event.description}</p>
 
-                    </div>
-                    <div>
-                        <Button
-                            asChild
-                            className="mb-2"
-                            size="lg"
-                        >
-                            <Link href={`/events/${event._id}`}>
-                                <span>Book Now</span>
-                            </Link>
-                        </Button>
-                        {event.url && (
-                            <>
-                                <div>
-                                    More about the event
-                                    <a
-                                        href={event.url}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        className="text-primary-600 hover:text-primary-800 transition underline ml-1"
-                                    >
-                                        Click here
-                                    </a>
-                                </div>
-                            </>
-                        )}
-                    </div>
+                </div>
+                <div>
+                    <Button
+                        asChild
+                        className="mb-2"
+                        size="lg"
+                    >
+                        <Link href={`/events/${event._id}`}>
+                            <span>Book Now</span>
+                        </Link>
+                    </Button>
+                    {event.url && (
+                        <>
+                            <div>
+                                More about the event
+                                <a
+                                    href={event.url}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="text-primary-600 hover:text-primary-800 transition underline ml-1"
+                                >
+                                    Click here
+                                </a>
+                            </div>
+                        </>
+                    )}
                 </div>
             </div>
         </section>
